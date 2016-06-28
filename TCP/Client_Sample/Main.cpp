ae26c71eaa36cf74633753d01898d7a01bc96c1a
Bousk <29147033+Bousk@users.noreply.github.com>
2016-06-28 19:42:13 +0100

    + protocole (TCP03)

--- TCP/Client_Sample/Main.cpp
+++ TCP/Client_Sample/Main.cpp
@@ -25,21 +25,20 @@ int main()
 		std::cout << "Entrez une phrase >";
 		std::string phrase;
 		std::getline(std::cin, phrase);
-		if ( client.Send(phrase.c_str(), phrase.length()) == SOCKET_ERROR )
+		if ( !client.Send((unsigned char*)phrase.c_str(), phrase.length()) )
 		{
 			std::cout << "Erreur envoi : " << Sockets::GetError() << std::endl;
 		}
 		else
 		{
-			char buffer[512] = {0};
-			int len = client.Receive(buffer, 512);
-			if ( len == SOCKET_ERROR )
+			std::vector<unsigned char> buffer;
+			if ( !client.Receive(buffer))
 			{
 				std::cout << "Erreur reception : " << Sockets::GetError() << std::endl;
 			}
 			else
 			{
-				std::string reply(buffer, len);
+				std::string reply((const char*)buffer.data(), buffer.size());
 				std::cout << "Reponse du serveur : " << reply << std::endl;
 			}
 		}
