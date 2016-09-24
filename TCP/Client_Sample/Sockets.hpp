a99df44c48223f00a626601f5fc11fcafbc0f411
Bousk <29147033+Bousk@users.noreply.github.com>
2016-09-24 19:50:38 +0100

    + missing files in C::B solutions + missing files in VS12 solution + missing files in makefile * fix C::B compile with GCC

--- TCP/Client_Sample/Sockets.hpp
+++ TCP/Client_Sample/Sockets.hpp
@@ -4,29 +4,29 @@
 #pragma once
 
 #ifdef _WIN32
-#if _MSC_VER >= 1800
-#include <WS2tcpip.h>
+	#if !defined(_MSC_VER) || _MSC_VER >= 1800
+		#include <WS2tcpip.h>
+	#else
+		#define inet_pton(FAMILY, IP, PTR_STRUCT_SOCKADDR) (*(PTR_STRUCT_SOCKADDR)) = inet_addr((IP))
+		typedef int socklen_t;
+	#endif
+	#include <WinSock2.h>
+	#ifdef _MSC_VER
+		#if _WIN32_WINNT == _WIN32_WINNT_WINBLUE
+			//!< Win8.1 & higher
+			#pragma comment(lib, "Ws2_32.lib")
+		#else
+			#pragma comment(lib, "wsock32.lib")
+		#endif
+	#endif
 #else
-#define inet_pton(FAMILY, IP, PTR_STRUCT_SOCKADDR) (*(PTR_STRUCT_SOCKADDR)) = inet_addr((IP))
-typedef int socklen_t;
-#endif
-#include <WinSock2.h>
-#ifdef _MSC_VER
-#if _WIN32_WINNT == _WIN32_WINNT_WINBLUE
-//!< Win8.1 & higher
-#pragma comment(lib, "Ws2_32.lib")
-#else
-#pragma comment(lib, "wsock32.lib")
-#endif
-#endif
-#else
-#include <sys/socket.h>
-#include <netinet/in.h> // sockaddr_in, IPPROTO_TCP
-#include <arpa/inet.h> // hton*, ntoh*, inet_addr
-#include <unistd.h>  // close
-#include <cerrno> // errno
-#define SOCKET int
-#define INVALID_SOCKET ((int)-1)
+	#include <sys/socket.h>
+	#include <netinet/in.h> // sockaddr_in, IPPROTO_TCP
+	#include <arpa/inet.h> // hton*, ntoh*, inet_addr
+	#include <unistd.h>  // close
+	#include <cerrno> // errno
+	#define SOCKET int
+	#define INVALID_SOCKET ((int)-1)
 #endif
 
 namespace Sockets
