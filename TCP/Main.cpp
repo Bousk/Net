9216c76abb5cd1f550870bb2c9293cdc9836a315
Bousk <Bousk@localhost>
2017-03-05 22:50:54 +0000

    + monothreaded server using select

--- TCP/Main.cpp
+++ TCP/Main.cpp
@@ -25,11 +25,11 @@ int main()
 		return -2;
 	}
 
-	if (!Sockets::SetNonBlocking(server))
-	{
-		std::cout << "Erreur settings non-bloquant : " << Sockets::GetError();
-		return -3;
-	}
+	//if (!Sockets::SetNonBlocking(server))
+	//{
+	//	std::cout << "Erreur settings non-bloquant : " << Sockets::GetError();
+	//	return -3;
+	//}
 
 	unsigned short port;
 	std::cout << "Port ? ";
@@ -58,56 +58,117 @@ int main()
 	std::vector<Client> clients;
 	for (;;)
 	{
-		sockaddr_in from = { 0 };
-		socklen_t addrlen = sizeof(from);
-		SOCKET newClientSocket = accept(server, (SOCKADDR*)(&from), &addrlen);
-		if (newClientSocket != INVALID_SOCKET)
 		{
-			if (!Sockets::SetNonBlocking(newClientSocket))
+			fd_set set;
+			timeval timeout = { 0 };
+			FD_ZERO(&set);
+			FD_SET(server, &set);
+			int selectReady = select(static_cast<int>(server) + 1, &set, nullptr, nullptr, &timeout);
+			if (selectReady == -1)
 			{
-				std::cout << "Erreur settings nouveau socket non-bloquant : " << Sockets::GetError() << std::endl;
-				Sockets::CloseSocket(newClientSocket);
-				continue;
+				std::cout << "Erreur select pour accept : " << Sockets::GetError() << std::endl;
+				break;
 			}
-			Client newClient;
-			newClient.sckt = newClientSocket;
-			newClient.addr = from;
-			const std::string clientAddress = Sockets::GetAddress(from);
-			const unsigned short clientPort = ntohs(from.sin_port);
-			std::cout << "Connexion de " << clientAddress.c_str() << ":" << clientPort << std::endl;
-			clients.push_back(newClient);
-		}
-		{
-			auto itClient = clients.begin();
-			while ( itClient != clients.end() )
+			if (selectReady > 0)
 			{
-				const std::string clientAddress = Sockets::GetAddress(from);
-				const unsigned short clientPort = ntohs(from.sin_port);
-				char buffer[200] = { 0 };
-				int ret = recv(itClient->sckt, buffer, 199, 0);
-				if (ret == 0)
+				sockaddr_in from = { 0 };
+				socklen_t addrlen = sizeof(from);
+				SOCKET newClientSocket = accept(server, (SOCKADDR*)(&from), &addrlen);
+				if (newClientSocket != INVALID_SOCKET)
 				{
-					//!< Déconnecté
-					std::cout << "Deconnexion de [" << clientAddress << ":" << clientPort << "]" << std::endl;
-					itClient = clients.erase(itClient);
-					continue;
+					//if (!Sockets::SetNonBlocking(newClientSocket))
+					//{
+					//	std::cout << "Erreur settings nouveau socket non-bloquant : " << Sockets::GetError() << std::endl;
+					//	Sockets::CloseSocket(newClientSocket);
+					//	continue;
+					//}
+					Client newClient;
+					newClient.sckt = newClientSocket;
+					newClient.addr = from;
+					const std::string clientAddress = Sockets::GetAddress(from);
+					const unsigned short clientPort = ntohs(from.sin_port);
+					std::cout << "Connexion de " << clientAddress.c_str() << ":" << clientPort << std::endl;
+					clients.push_back(newClient);
 				}
-				if (ret == SOCKET_ERROR)
+			}
+		}
+		if (!clients.empty())
+		{
+			fd_set setReads;
+			fd_set setWrite;
+			fd_set setErrors;
+			FD_ZERO(&setReads);
+			FD_ZERO(&setWrite);
+			FD_ZERO(&setErrors);
+			int highestFd = 0;
+			timeval timeout = { 0 };
+			for (auto& client : clients)
+			{
+				FD_SET(static_cast<int>(client.sckt), &setReads);
+				FD_SET(static_cast<int>(client.sckt), &setWrite);
+				FD_SET(static_cast<int>(client.sckt), &setErrors);
+				if (client.sckt > highestFd)
+					highestFd = static_cast<int>(client.sckt);
+			}
+			int selectResult = select(highestFd + 1, &setReads, &setWrite, &setErrors, &timeout);
+			if (selectResult == -1)
+			{
+				std::cout << "Erreur select pour clients : " << Sockets::GetError() << std::endl;
+				break;
+			}
+			else if (selectResult > 0)
+			{
+				auto itClient = clients.begin();
+				while (itClient != clients.end())
 				{
-					int error = Sockets::GetError();
-					if (error != static_cast<int>(Sockets::Errors::WOULDBLOCK))
+					const std::string clientAddress = Sockets::GetAddress(itClient->addr);
+					const unsigned short clientPort = ntohs(itClient->addr.sin_port);
+
+					bool hasError = false;
+					if (FD_ISSET(itClient->sckt, &setErrors))
 					{
+						std::cout << "Erreur" << std::endl;
+						hasError = true;
+					}
+					else if (FD_ISSET(itClient->sckt, &setReads))
+					{
+						char buffer[200] = { 0 };
+						int ret = recv(itClient->sckt, buffer, 199, 0);
+						if (ret == 0)
+						{
+							std::cout << "Connexion terminee" << std::endl;
+							hasError = true;
+						}
+						else if(ret == SOCKET_ERROR)
+						{
+							std::cout << "Erreur reception : " << Sockets::GetError() << std::endl;
+							hasError = true;
+						}
+						else
+						{
+							std::cout << "[" << clientAddress << ":" << clientPort << "]" << buffer << std::endl;
+							if (FD_ISSET(itClient->sckt, &setWrite))
+							{
+								ret = send(itClient->sckt, buffer, ret, 0);
+								if (ret == 0 || ret == SOCKET_ERROR)
+								{
+									std::cout << "Erreur envoi" << Sockets::GetError() << std::endl;
+									hasError = true;
+								}
+							}
+						}
+					}
+					if (hasError)
+					{
+						//!< Déconnecté
 						std::cout << "Deconnexion de [" << clientAddress << ":" << clientPort << "]" << std::endl;
 						itClient = clients.erase(itClient);
-						continue;
 					}
-					//!< il n'y avait juste rien à recevoir, on passe au suivant
-					continue;
+					else
+					{
+						++itClient;
+					}
 				}
-				std::cout << "[" << clientAddress << ":" << clientPort << "]" << buffer << std::endl;
-				ret = send(itClient->sckt, buffer, ret, 0);
-				if (ret == 0 || ret == SOCKET_ERROR)
-					break;
 			}
 		}
 	}
