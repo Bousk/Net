0faf5f3b5d12d58612a0adce900debccb2d7517b
Bousk <Bousk@localhost>
2017-10-17 21:56:09 +0100

    + add id to Datagram + Datagram structure

---
+++ NetworkLib/src/UDP/Datagram.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdint>
+#include <array>
+
+namespace Bousk
+{
+	namespace UDP
+	{
+		struct Datagram
+		{
+			using ID = uint16_t;
+			static constexpr uint8_t BufferMaxSize = 1400;
+			static constexpr uint8_t DataMaxSize = BufferMaxSize - sizeof(ID);
+
+			ID id;
+			std::array<uint8_t, DataMaxSize> data;
+		};
+	}
+}
