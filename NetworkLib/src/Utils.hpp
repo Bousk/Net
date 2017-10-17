2d7d9488896464d384b37662f0de15843c2b63f2
Bousk <Bousk@localhost>
2017-10-17 22:11:00 +0100

    + sequence checking

--- NetworkLib/src/Utils.hpp
+++ NetworkLib/src/Utils.hpp
@@ -1,3 +1,17 @@
 #pragma once
 
+#include <cstdint>
+#include <chrono>
+
 #define UNUSED(x) (void)(x)
+
+namespace Bousk
+{
+	namespace Utils
+	{
+		std::chrono::milliseconds Now();
+
+		bool IsSequenceNewer(uint32_t sNew, uint32_t sLast);
+		uint32_t SequenceDiff(uint32_t sNew, uint32_t sLast);
+	}
+}
