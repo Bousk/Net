d062af232d722a0d45d5eef0a958140814c3f29f
Bousk <29147033+Bousk@users.noreply.github.com>
2018-07-01 09:13:28 +0100

    + UDP Client & Distant Client

---
+++ NetworkLib/src/UDP/DistantClient.cpp
@@ -0,0 +1,77 @@
+#include "UDP/DistantClient.hpp"
+#include "UDP/UDPClient.hpp"
+#include "UDP/Datagram.hpp"
+#include "Messages.hpp"
+
+namespace Bousk
+{
+	namespace Network
+	{
+		namespace UDP
+		{
+			DistantClient::DistantClient(Client& client, const sockaddr_storage& addr)
+				: mClient(client)
+			{
+				memcpy(&mAddress, &addr, sizeof(addr));
+			}
+			void DistantClient::send(std::vector<uint8_t>&& data)
+			{
+				Datagram datagram;
+				datagram.header.id = htons(mNextDatagramIdToSend);
+				++mNextDatagramIdToSend;
+				datagram.header.ack = htons(mReceivedAcks.lastAck());
+				datagram.header.previousAcks = mReceivedAcks.previousAcksMask();
+				memcpy(datagram.data.data(), data.data(), data.size());
+
+				sendto(mClient.mSocket, reinterpret_cast<const char*>(&datagram), static_cast<int>(Datagram::HeaderSize + data.size()), 0, reinterpret_cast<const sockaddr*>(&mAddress), sizeof(mAddress));
+			}
+			void DistantClient::onDatagramReceived(Datagram&& datagram)
+			{
+				const auto datagramid = ntohs(datagram.header.id);
+				//!< Update the received acks tracking
+				mReceivedAcks.update(datagramid, 0, true);
+				//!< Update the send acks tracking
+				mSentAcks.update(ntohs(datagram.header.ack), datagram.header.previousAcks, true);
+				//!< Ignore duplicate
+				if (!mReceivedAcks.isNewlyAcked(datagramid))
+					return;
+				//!< Handle loss on reception
+				const auto lostDatagrams = mReceivedAcks.loss();
+				for (const auto receivedLostDatagram : lostDatagrams)
+				{
+					onDatagramReceivedLost(receivedLostDatagram);
+				}
+				//!< Handle loss on send
+				const auto datagramsSentLost = mSentAcks.loss();
+				for (const auto sendLoss : datagramsSentLost)
+				{
+					onDatagramSentLost(sendLoss);
+				}
+				//!< Mark new send acked
+				const auto datagramsSentAcked = mSentAcks.getNewAcks();
+				for (const auto sendAcked : datagramsSentAcked)
+				{
+					onDatagramSentAcked(sendAcked);
+				}
+				//!< Dispatch data
+				onDataReceived(std::vector<uint8_t>(datagram.data.data(), datagram.data.data() + datagram.datasize));
+			}
+
+			void DistantClient::onDatagramSentAcked(Datagram::ID datagramId)
+			{}
+			void DistantClient::onDatagramSentLost(Datagram::ID datagramId)
+			{}
+			void DistantClient::onDatagramReceivedLost(Datagram::ID datagramId)
+			{}
+			void DistantClient::onDataReceived(std::vector<uint8_t>&& data)
+			{
+				onMessageReady(std::make_unique<Messages::UserData>(std::move(data)));
+			}
+			void DistantClient::onMessageReady(std::unique_ptr<Messages::Base>&& msg)
+			{
+				memcpy(&(msg->from), &mAddress, sizeof(mAddress));
+				mClient.onMessageReady(std::move(msg));
+			}
+		}
+	}
+}
---
+++ NetworkLib/src/UDP/UDPClient.cpp
@@ -0,0 +1,106 @@
+#include "UDP/UDPClient.hpp"
+#include "UDP/DistantClient.hpp"
+#include "Messages.hpp"
+#include "Errors.hpp"
+
+#include <algorithm>
+
+namespace Bousk
+{
+	namespace Network
+	{
+		namespace UDP
+		{
+			Client::Client()
+			{}
+			Client::~Client()
+			{}
+
+			bool Client::init(uint16_t port)
+			{
+				release();
+				mSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+				if (mSocket == INVALID_SOCKET)
+					return false;
+
+				sockaddr_in addr;
+				addr.sin_addr.s_addr = INADDR_ANY;
+				addr.sin_port = htons(port);
+				addr.sin_family = AF_INET;
+				int res = bind(mSocket, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+				if (res != 0)
+					return false;
+
+				if (!SetNonBlocking(mSocket))
+					return false;
+
+				return true;
+			}
+			void Client::release()
+			{
+				if (mSocket != INVALID_SOCKET)
+					CloseSocket(mSocket);
+				mSocket = INVALID_SOCKET;
+			}
+			void Client::sendTo(const sockaddr_storage& target, std::vector<uint8_t>&& data)
+			{
+				auto& client = getClient(target);
+				client.send(std::move(data));
+			}
+			void Client::receive()
+			{
+				for (;;)
+				{
+					Datagram datagram;
+					sockaddr_in from{ 0 };
+					socklen_t fromlen = sizeof(from);
+					int ret = recvfrom(mSocket, reinterpret_cast<char*>(&datagram), Datagram::BufferMaxSize, 0, reinterpret_cast<sockaddr*>(&from), &fromlen);
+					if (ret > 0)
+					{
+						if (ret > Datagram::HeaderSize)
+						{
+							datagram.datasize = ret - Datagram::HeaderSize;
+							auto& client = getClient(reinterpret_cast<sockaddr_storage&>(from));
+							client.onDatagramReceived(std::move(datagram));
+						}
+						else
+						{
+							//!< Something is wrong, unexpected datagram
+						}
+					}
+					else
+					{
+						if (ret < 0)
+						{
+							//!< Error handling
+							const auto err = Errors::Get();
+							if (err != Errors::WOULDBLOCK)
+							{
+								//!< Log that error
+							}
+						}
+						return;
+					}
+				}
+			}
+			std::vector<std::unique_ptr<Messages::Base>>&& Client::poll()
+			{
+				return std::move(mMessages);
+			}
+
+			DistantClient& Client::getClient(const sockaddr_storage& clientAddr)
+			{
+				auto itClient = std::find_if(mClients.begin(), mClients.end(), [&](DistantClient& client) { return memcmp(&(client.address()), &clientAddr, sizeof(sockaddr_storage)); });
+				if (itClient != mClients.end())
+					return *itClient;
+
+				mClients.emplace_back(*this, clientAddr);
+				return mClients.back();
+			}
+			void Client::onMessageReady(std::unique_ptr<Messages::Base>&& msg)
+			{
+				mMessages.push_back(std::move(msg));
+			}
+		}
+	}
+}
