b9e09320cfb3404bfbde28d95fcff59a6e58be90
Bousk <Bousk@localhost>
2018-07-01 09:13:28 +0100

    + UDP Client & Distant Client

--- NetworkLib/src/UDP/Datagram.hpp
+++ NetworkLib/src/UDP/Datagram.hpp
@@ -19,10 +19,13 @@ namespace Bousk
 					uint64_t previousAcks;
 				};
 				static constexpr size_t BufferMaxSize = 1400;
-				static constexpr size_t DataMaxSize = BufferMaxSize - sizeof(Header);
+				static constexpr size_t HeaderSize = sizeof(Header);
+				static constexpr size_t DataMaxSize = BufferMaxSize - HeaderSize;
 
 				Header header;
 				std::array<uint8_t, DataMaxSize> data;
+
+				size_t datasize{ 0 };
 			};
 		}
 	}
