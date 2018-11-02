dac4f3ed9b0af0b0fc88b3097713b43c301ff2ba
Bousk <Bousk@localhost>
2018-11-02 00:45:46 -0400

    + process send to send queued data > send now only queues data

--- NetworkLib/src/UDP/DistantClient.cpp
+++ NetworkLib/src/UDP/DistantClient.cpp
@@ -19,14 +19,24 @@ namespace Bousk
 			}
 			void DistantClient::send(std::vector<uint8_t>&& data)
 			{
+				mSendQueue.queue(std::move(data));
+			}
+			void DistantClient::processSend()
+			{
 				Datagram datagram;
-				datagram.header.id = htons(mNextDatagramIdToSend);
-				++mNextDatagramIdToSend;
 				datagram.header.ack = htons(mReceivedAcks.lastAck());
 				datagram.header.previousAcks = mReceivedAcks.previousAcksMask();
-				memcpy(datagram.data.data(), data.data(), data.size());
+				for (;;)
+				{
+					const auto serializedSize = mSendQueue.serialize(datagram.data.data(), Datagram::DataMaxSize);
+					if (serializedSize == 0)
+						break;
 
-				sendto(mClient.mSocket, reinterpret_cast<const char*>(&datagram), static_cast<int>(Datagram::HeaderSize + data.size()), 0, reinterpret_cast<const sockaddr*>(&mAddress), sizeof(mAddress));
+					datagram.header.id = htons(mNextDatagramIdToSend);
+					++mNextDatagramIdToSend;
+
+					sendto(mClient.mSocket, reinterpret_cast<const char*>(&datagram), static_cast<int>(Datagram::HeaderSize + serializedSize), 0, reinterpret_cast<const sockaddr*>(&mAddress), sizeof(mAddress));
+				}
 			}
 			void DistantClient::onDatagramReceived(Datagram&& datagram)
 			{
