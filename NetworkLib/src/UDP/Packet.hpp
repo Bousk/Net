762e1e1924fde37a3fb0676767a531be958130dd
Bousk <Bousk@localhost>
2018-11-10 13:25:19 -0500

    * fix packet serialization > handle header...

--- NetworkLib/src/UDP/Packet.hpp
+++ NetworkLib/src/UDP/Packet.hpp
@@ -27,16 +27,21 @@ namespace Bousk
 					Type type;
 				};
 				static constexpr size_t PacketMaxSize = Datagram::DataMaxSize;
-				static constexpr size_t DataMaxSize = PacketMaxSize - sizeof(Header);
+				static constexpr size_t HeaderSize = sizeof(Header);
+				static constexpr size_t DataMaxSize = PacketMaxSize - HeaderSize;
 
 				Header header;
-				std::array<uint8_t, DataMaxSize> buffer;
+				std::array<uint8_t, DataMaxSize> dataBuffer;
 
 				Id id() const { return header.id; }
 				Type type() const { return header.type; }
-				uint8_t* data() { return buffer.data(); }
-				const uint8_t* data() const { return buffer.data(); }
-				inline uint16_t size() const { return header.size; }
+				uint8_t* data() { return dataBuffer.data(); }
+				const uint8_t* data() const { return dataBuffer.data(); }
+				inline uint16_t datasize() const { return header.size; }
+				// Full buffer : header + data
+				inline const uint8_t* buffer() const { return reinterpret_cast<const uint8_t*>(this); }
+				// Packet full size : header + data
+				inline uint16_t size() const { return HeaderSize + header.size; }
 			};
 		}
 	}
