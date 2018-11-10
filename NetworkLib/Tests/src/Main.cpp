84e9631c43cfd2195c8c98433597af6b264ccc13
Bousk <29147033+Bousk@users.noreply.github.com>
2018-11-10 13:27:17 -0500

    * first test for multiplexer > serialize data

--- NetworkLib/Tests/src/Main.cpp
+++ NetworkLib/Tests/src/Main.cpp
@@ -1,11 +1,13 @@
 #include "Utils_Test.hpp"
 #include "AckHandler_Test.hpp"
 #include "DistantClient_Test.hpp"
+#include "PacketHandling_Test.hpp"
 
 int main()
 {
 	Utils_Test::Test();
 	AckHandler_Test::Test();
+	PacketHandling_Test::Test();
 	DistantClient_Test::Test();
 	return 0;
 }
