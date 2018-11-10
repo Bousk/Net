f26c598d6a1f410dcf4a117b0b23c67bcf493a24
Bousk <Bousk@localhost>
2018-11-10 13:27:17 -0500

    * first test for multiplexer > serialize data

--- NetworkLib/Tests/src/PacketHandling_Test.hpp
+++ NetworkLib/Tests/src/PacketHandling_Test.hpp
@@ -12,6 +12,22 @@ public:
 void Multiplexer_Test::Test()
 {
 	Bousk::Network::UDP::Multiplexer mux;
+	CHECK(mux.mQueue.size() == 0);
+	CHECK(mux.mNextId == 0);
+	{
+		std::array<uint8_t, 5> arr{ 'T', 'o', 't', 'o', '\0' };
+		std::vector<uint8_t> data(arr.cbegin(), arr.cend());
+		mux.queue(std::move(data));
+		CHECK(mux.mQueue.size() == 1);
+		CHECK(mux.mNextId == 1);
+
+		std::array<uint8_t, Bousk::Network::UDP::Packet::PacketMaxSize> buffer;
+		size_t serializedData = mux.serialize(buffer.data(), buffer.size());
+		CHECK(serializedData == Bousk::Network::UDP::Packet::HeaderSize + arr.size());
+		const Bousk::Network::UDP::Packet* packet = reinterpret_cast<const Bousk::Network::UDP::Packet*>(buffer.data());
+		CHECK(packet->datasize() == arr.size());
+		CHECK(memcmp(packet->data(), arr.data(), packet->datasize()) == 0);
+	}
 }
 
 class Demultiplexer_Test
@@ -24,3 +40,13 @@ void Demultiplexer_Test::Test()
 {
 	Bousk::Network::UDP::Demultiplexer demux;
 }
+
+class PacketHandling_Test
+{
+public:
+	static void Test()
+	{
+		Multiplexer_Test::Test();
+		Demultiplexer_Test::Test();
+	}
+};
