624fe8d2803e74a18c7a5a1c02f9716c8cac302d
Bousk <Bousk@localhost>
2018-11-12 00:41:41 -0500

    + add debug into debugger when a test fails > VS only for now * remove display of successfull tests > less spammy output, easier to get failures

--- NetworkLib/Tests/src/Tester.hpp
+++ NetworkLib/Tests/src/Tester.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <iostream>
+#ifdef _MSC_VER
+#define DEBUGBREAK() __debugbreak()
+#else
+// TODO
+#define DEBUGBREAK() 
+#endif
 
 #define TO_STRING_INTERNAL(token) #token
 #define TO_STRING(token) TO_STRING_INTERNAL(token)
-#define CHECK_SUCCESS(expr) do { std::cout << "[OK] " #expr << std::endl; } while(0)
-#define CHECK_FAILURE(expr) do { std::cout << "[FAILURE][" __FILE__ ":" TO_STRING(__LINE__) "] " #expr << std::endl; } while(0)
+#define CHECK_SUCCESS(expr) do { /*std::cout << "[OK] " #expr << std::endl;*/ } while(0)
+#define CHECK_FAILURE(expr) do { std::cout << "[FAILURE][" __FILE__ ":" TO_STRING(__LINE__) "] " #expr << std::endl; DEBUGBREAK(); } while(0)
 #define CHECK(expr) do { if (expr) { CHECK_SUCCESS(expr); } else { CHECK_FAILURE(expr); } } while(0)
