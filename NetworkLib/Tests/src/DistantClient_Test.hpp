9c9a3fab0a17d29e3093d65500357548b3f434b8
Bousk <29147033+Bousk@users.noreply.github.com>
2019-02-04 21:45:58 -0500

    * update & clean DistantClient UT

--- NetworkLib/Tests/src/DistantClient_Test.hpp
+++ NetworkLib/Tests/src/DistantClient_Test.hpp
@@ -33,15 +33,17 @@ void DistantClient_Test::Test()
 
 	constexpr const char TestString[] = "Test data";
 	constexpr size_t TestStringLength = sizeof(TestString);
-	distantClient.send(std::vector<uint8_t>(TestString, TestString + TestStringLength));
 
+	distantClient.send(std::vector<uint8_t>(TestString, TestString + TestStringLength));
 	//!< Craft the datagram to check reception
 	Bousk::Network::UDP::Datagram datagram;
 	distantClient.fillDatagram(datagram);
-
-	// Process send to update internal state
-	distantClient.processSend();
 	CHECK(distantClient.mNextDatagramIdToSend == 1);
+
+	auto QueueDatagram = [&]() {
+		distantClient.send(std::vector<uint8_t>(TestString, TestString + TestStringLength));
+		distantClient.mSendQueue.serialize(datagram.data.data(), Bousk::Network::UDP::Datagram::DataMaxSize);
+	};
 
 	CHECK(datagram.header.id == 0);
 	CHECK(datagram.datasize == TestStringLength + Bousk::Network::UDP::Packet::HeaderSize);
@@ -71,8 +73,9 @@ void DistantClient_Test::Test()
 		CHECK(polledMessages.size() == 0);
 	}
 
-	//!< Send datagram #2, 1 is now missing
+	//!< Receive datagram #2, #1 is now missing
 	datagram.header.id = htons(2);
+	QueueDatagram();
 	{
 		distantClient.onDatagramReceived(std::move(datagram));
 		CHECK(distantClient.mReceivedAcks.lastAck() == 2);
@@ -89,6 +92,7 @@ void DistantClient_Test::Test()
 
 	//!< Now receive datagram #1
 	datagram.header.id = htons(1);
+	QueueDatagram();
 	{
 		distantClient.onDatagramReceived(std::move(datagram));
 		CHECK(distantClient.mReceivedAcks.lastAck() == 2);
@@ -107,6 +111,7 @@ void DistantClient_Test::Test()
 
 	//!< Jump 64 packets ahead, all missed in between
 	datagram.header.id = htons(66);
+	QueueDatagram();
 	{
 		distantClient.onDatagramReceived(std::move(datagram));
 		CHECK(distantClient.mReceivedAcks.lastAck() == 66);
@@ -125,6 +130,7 @@ void DistantClient_Test::Test()
 
 	//!< Receive next one and everything is missing in between
 	datagram.header.id = htons(67);
+	QueueDatagram();
 	{
 		distantClient.onDatagramReceived(std::move(datagram));
 		CHECK(distantClient.mReceivedAcks.lastAck() == 67);
@@ -141,8 +147,9 @@ void DistantClient_Test::Test()
 		CHECK(memcmp(TestString, dataMsg->data.data(), TestStringLength) == 0);
 	}
 
-	//!< Receive next one, 3 is now lost
+	//!< Receive next one, #3 is now lost
 	datagram.header.id = htons(68);
+	QueueDatagram();
 	{
 		distantClient.onDatagramReceived(std::move(datagram));
 		CHECK(distantClient.mReceivedAcks.lastAck() == 68);
@@ -160,6 +167,7 @@ void DistantClient_Test::Test()
 
 	//!< Receive datagram #3 : too old, ignored
 	datagram.header.id = htons(3);
+	QueueDatagram();
 	{
 		distantClient.onDatagramReceived(std::move(datagram));
 		CHECK(distantClient.mReceivedAcks.lastAck() == 68);
