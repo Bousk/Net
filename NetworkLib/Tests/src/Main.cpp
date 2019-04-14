3412fda94ac7fd8886516dd6c8bb728af40703ee
Bousk <Bousk@localhost>
2019-04-14 19:18:30 -0400

    + reliable ordered protocole

--- NetworkLib/Tests/src/Main.cpp
+++ NetworkLib/Tests/src/Main.cpp
@@ -2,6 +2,7 @@
 #include "AckHandler_Test.hpp"
 #include "DistantClient_Test.hpp"
 #include "PacketHandling_Test.hpp"
+#include "ReliableOrdered_Test.hpp"
 
 int main()
 {
@@ -9,5 +10,6 @@ int main()
 	AckHandler_Test::Test();
 	PacketHandling_Test::Test();
 	DistantClient_Test::Test();
+	ReliableOrdered_Test::Test();
 	return 0;
 }
