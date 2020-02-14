467361df955beff71c2d392bb402d7b04fb35e88
Bousk <bousk@supinfo.com>
2020-02-13 19:31:00 -0500

    * fix default timeout

--- NetworkLib/src/Settings.hpp
+++ NetworkLib/src/Settings.hpp
@@ -5,4 +5,4 @@
 // Enable serialization of float32
 //#define BOUSKNET_ALLOW_FLOAT32_SERIALIZATION
 
-#define DEFAULT_UDP_TIMEOUT std::chrono::milliseconds(5)
+#define DEFAULT_UDP_TIMEOUT std::chrono::seconds(1)
